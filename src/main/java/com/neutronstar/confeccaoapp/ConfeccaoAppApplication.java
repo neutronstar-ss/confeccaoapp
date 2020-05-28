3de77286b2dd3f3372c0720706ad68de0a3a04bc
neutronstar-ss <neutron.star.system.services@gmail.com>
2020-05-28 17:59:36 -0300

    first commit

---
+++ src/main/java/com/neutronstar/confeccaoapp/ConfeccaoAppApplication.java
@@ -0,0 +1,13 @@
+package com.neutronstar.confeccaoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfeccaoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfeccaoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neutronstar/confeccaoapp/ConfeccaoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neutronstar.confeccaoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfeccaoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
